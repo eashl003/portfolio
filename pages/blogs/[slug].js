77a80da0b812feffbf6bebdca0ed0aba8786e7d7
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-02-02 07:13:49 -0500

    added article and blogs

---
+++ pages/blogs/[slug].js
@@ -0,0 +1,39 @@
+import Head from "next/head"
+import { useRouter } from "next/router"
+import ArticlesList from "../../components/ArticlesList"
+import { getBlogs, getBlog } from "../../utils/api"
+
+const BlogPage = ({ blog }) => {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <div>Loading category...</div>
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>{blog.name} products</title>
+      </Head>
+      <ArticlesList articles={blog.articles} />
+    </div>
+  )
+}
+
+export default BlogPage
+
+export async function getStaticProps({ params }) {
+  const blog = await getBlog(params.slug)
+  return { props: { blog } }
+}
+
+export async function getStaticPaths() {
+  const blogs = await getBlogs()
+  return {
+    paths: blogs.map((_blog) => {
+      return {
+        params: { slug: _blog.slug },
+      }
+    }),
+    fallback: true,
+  }
+}
