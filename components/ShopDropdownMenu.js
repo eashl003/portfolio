4b97e8cf129ebdc7bb2c5b969926ac728a7099ce
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-02-03 07:28:08 -0500

    test

--- components/ShopDropdownMenu.js
+++ components/ShopDropdownMenu.js
@@ -10,7 +10,7 @@ const ShopDropdownMenu = ({ categories = [] }) => {
     <Menu as="div" className="z-10 relative inline-block text-left">
         <div>
             <Menu.Button className="uppercase inline-flex justify-center text-teal-800 px-4 py-2 outline-none focus:outline-none hover:text-teal-600">
-                Shop
+                Shopp
             <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
             </Menu.Button>
         </div>
