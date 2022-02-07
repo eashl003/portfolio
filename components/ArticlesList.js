dafd8c5ea144a9df4df3788928a46424b6150a1c
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-02-07 06:23:05 -0500

    updated blog post page

--- components/ArticlesList.js
+++ components/ArticlesList.js
@@ -4,10 +4,10 @@ import Image from "next/image"
 
 const ArticlesList = ({ articles }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 sm:gap-10 items-center justify-center mt-10">
+    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 items-center justify-center mt-10 ml-10 mr-10">
       {articles.map((_article) => (
         <div key={_article.id}>
-          <div className="max-w-md mx-auto bg-white rounded-xl shadow-xl overflow-hidden md:max-w-3xl my-5 transform translate-y-1 hover:translate-y-3 duration-500 ease-in-out">
+          <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-3xl my-5 transform translate-y-1 hover:translate-y-3 duration-500 ease-in-out">
             <div className="md:flex">
               <div className="items-stretch">
                 <div className="h-48 w-48 relative">
@@ -23,11 +23,11 @@ const ArticlesList = ({ articles }) => {
               <div className="p-8">
                 <a
                   href="#"
-                  className="block mt-1 text-2xl leading-tight font-medium text-black"
+                  className="block mt-1 text-lg leading-tight font-medium text-black"
                 >
                   {_article.title}
                 </a>
-                <p className="mt-2 text-xl text-gray-500 line-clamp-4">
+                <p className="mt-2 text-sm text-gray-500 line-clamp-4">
                   {_article.description}
                 </p>
               </div>
