f7e1dbe95ed6da3074422f91866c473aef86e0a5
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-04-11 16:30:29 -0400

    updated recent articles

--- components/RecentArticlesList.js
+++ components/RecentArticlesList.js
@@ -4,13 +4,13 @@ import NextImage from "./Image"
 // throw and exeception if value doesn't exist...
 const RecentArticlesList = ({ recentArticles }) => {
   return (
-    <div>
+    <div className="">
       <div>
         <h1 className="flex justify-center items-center text-2xl  font-medium font-gravitas text-5xl pt-20 text-gray-900">
           Recent Posts
         </h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-y-16 items-center justify-center mt-10">
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-y-16 items-center justify-center mt-10 absolute m-auto left-0 right-0">
         {recentArticles.slice(0, 2).map((_recentArticle) => (
           <div key={_recentArticle.id}>
             <div className="max-w-md mx-auto bg-transparent rounded shadow-md overflow-hidden md:max-w-6xl transform hover:scale-105 duration-500 ease-in-out hover:shadow-xl">
