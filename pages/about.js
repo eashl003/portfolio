4d13f2c613e9b25c4d832275dd1a9473db31a428
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-06-12 10:32:31 -0400

    updated about

--- pages/about.js
+++ pages/about.js
@@ -4,15 +4,24 @@ import ThemeButton from "../components/ThemeButton"
 
 const About = ({ about }) => {
   return (
-    <div className="bg-slate-900 flex flex-col h-screen text-center text-teal-100">
-      <section className="">
+    <div className="grid grid-cols-2 h-screen bg-slate-900 text-white">
+      <section className="mt-48 ml-40">
         <h1 className="text-3xl">
           Elisabeth Ashley<span className="text-xl"> (She/Her)</span>
         </h1>
         <h1 className="text-xl">
           Oracle APEX Developer at Insum Solutions a Talan Company
         </h1>
-        <ThemeButton title="Email" />
+        <div className="align-middle">
+          <ThemeButton title="Email" />
+        </div>
+      </section>
+      <section className="">
+        <h1 className="underline underline-offset-1 text-3xl">About Me</h1>
+        <div className="flex flex-row items-center">
+          <ThemeButton title="Resume" />
+          <ThemeButton title="Certifications" />
+        </div>
       </section>
     </div>
   )
