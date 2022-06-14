cadd439a7326a575d327a3c973e439ebfa21c580
Lizzy <62777652+eashl003@users.noreply.github.com>
2022-06-14 07:01:01 -0400

    Added email form

--- components/EmailModal.js
+++ components/EmailModal.js
@@ -11,27 +11,94 @@ export default function EmailModal(props) {
           <div className="flex justify-center items-center fixed inset-0 z-50">
             <div className="max-w-7xl max-h-screen">
               <div className="border-0 rounded-lg shadow-lg relative w-full bg-white">
-                <div className="flex justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold text-black">Email</h3>
+                <div className="p-4 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold text-black">
+                    Contact Me
+                  </h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={action}
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    <span className="-m-10 bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                       ×
                     </span>
                   </button>
                 </div>
-                <div className="relative p-6 flex-auto overflow-y-scroll w-120 h-110 text-black">
-                  <div>Email</div>
+                <div className="relative p-6 flex justify-center overflow-y-hidden w-110 h-100 text-black">
+                  <div>
+                    <form className="w-full max-w-lg">
+                      <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            First Name
+                          </label>
+                          <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                            id="grid-first-name"
+                            type="text"
+                            placeholder="Jane"
+                          />
+                          <p className="text-red-500 text-xs italic">
+                            Please fill out this field.
+                          </p>
+                        </div>
+                        <div className="w-full md:w-1/2 px-3">
+                          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            Last Name
+                          </label>
+                          <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-last-name"
+                            type="text"
+                            placeholder="Doe"
+                          />
+                        </div>
+                      </div>
+                      <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full px-3">
+                          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            E-mail
+                          </label>
+                          <input
+                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="email"
+                            type="email"
+                          />
+                          <p className="text-gray-600 text-xs italic">
+                            Some tips - as long as needed
+                          </p>
+                        </div>
+                      </div>
+                      <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full px-3">
+                          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            Message
+                          </label>
+                          <textarea
+                            className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+                            id="message"
+                          ></textarea>
+                        </div>
+                      </div>
+                      <div className="md:flex md:items-center">
+                        <div className="md:w-2/3"></div>
+                      </div>
+                    </form>
+                  </div>
                 </div>
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                <div className="flex justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={action}
                   >
                     Close
+                  </button>
+                  <button
+                    className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-8 rounded"
+                    type="button"
+                  >
+                    SEND
                   </button>
                 </div>
               </div>
